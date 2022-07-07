23099588cb2e91d7d57b51eeefe1f702631734f2
Luis Jose Enrique Ramos Mercedes <luis_ramos1@ucne.edu.do>
2022-07-07 11:52:34 -0400

    Testing Controllers.

--- src/test/java/com/lj/restapi/controller/StudentControllerTest.java
+++ src/test/java/com/lj/restapi/controller/StudentControllerTest.java
@@ -6,59 +6,51 @@ import com.lj.restapi.entity.Career;
 import com.lj.restapi.entity.Gender;
 import com.lj.restapi.entity.Student;
 import com.lj.restapi.repository.StudentRepository;
+import com.lj.restapi.service.StudentService;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@WebMvcTest(StudentController.class)
 class StudentControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
-    ObjectMapper objectMapper = new ObjectMapper();
-    ObjectWriter objectWriter = objectMapper.writer();
+    @MockBean
+    private StudentService studentService;
 
-    @Mock
-    private StudentRepository studentRepository;
+    Student student = new Student(1L, "luis", "ramos", "lj@ramos.com", 24, Gender.MALE, Career.Software_Developer);
 
-    @InjectMocks
-    private StudentController studentController;
-
-    Student student1 = new Student(1, "Luis", "Ramos", "lj_erm@hotmail.com", 24, Gender.MALE, Career.Software_Developer);
-    Student student2 = new Student(2, "Jose", "Mercedes", "jm@hotmail.com", 23, Gender.MALE, Career.Data_Analyst);
-    Student student3 = new Student(3, "Kath", "Smith", "ks@hotmail.com", 22, Gender.FEMALE, Career.Architecture);
-
-    @BeforeEach
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
-        this.mockMvc = standaloneSetup(studentController).build();
+    private void setStudent(){
+        studentService.saveStudent(student);
     }
 
     @Test
-    public void canGetAllStudents() throws Exception{
-        List<Student> students = new ArrayList<>(Arrays.asList(student1,student2,student3));
-
-        when(studentRepository.findAll()).thenReturn(students);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/students")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)));
-
+    void shouldGetStudentById() {
+        setStudent();
+        when(studentService.getStudentById(1L)).thenReturn(student);
     }
 }
